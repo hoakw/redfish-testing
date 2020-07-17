d8c5344848e57dcd42ed122520724389960d9fb3
hoakw <hoakw12@gmail.com>
2020-07-16 23:10:59 -0700

    Upload: Change test script

--- test_script.sh
+++ test_script.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+./up_server.sh &
+
+sleep 4
 
 echo "TEST Start - Get Data"
 
 curl http://localhost:8000/redfish/v1
+
+
+
+echo "TEST Finish!!!"
+
+kill -9 $(ps -ef | grep redfishMockup | awk '{ print $2 }')
--- up_server.sh
+++ up_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip3.5 install grequests
+#pip3.5 install grequests
 
 python3 redfishMockupServer.py -D ./test 
 
